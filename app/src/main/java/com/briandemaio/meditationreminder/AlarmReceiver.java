d29f3348db5d4e9045b5919ecd167e737690153a
Brian De Maio <bdemaio1@gmail.com>
2019-08-10 16:55:15 -0400

    First push for this proj

---
+++ app/src/main/java/com/briandemaio/meditationreminder/AlarmReceiver.java
@@ -0,0 +1,42 @@
+package com.briandemaio.meditationreminder;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import androidx.core.app.NotificationCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private NotificationManager mNotificationManager;
+    public static String NOTIFICATION_ID = "notification-id";
+    public static String NOTIFICATION = "notification";
+    private int mId;
+    private String mMessage;
+    private static final String PRIMARY_CHANNEL_ID =
+            "primary_notification_channel";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        mNotificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+         mId = intent.getIntExtra(NOTIFICATION_ID, 1);
+         mMessage = intent.getStringExtra(NOTIFICATION);
+        deliverNotification(context);
+    }
+
+    private void deliverNotification(Context context) {
+        Intent contentIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (context, mId, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, PRIMARY_CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_duck_and_bottle_small)
+                .setContentText(mMessage)
+                .setContentIntent(contentPendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setAutoCancel(true)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+        mNotificationManager.notify(mId, builder.build());
+    }
+}
---
+++ app/src/main/java/com/briandemaio/meditationreminder/FirstTimeFragment.java
@@ -0,0 +1,44 @@
+package com.briandemaio.meditationreminder;
+
+import android.os.Bundle;
+import androidx.fragment.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+// ...
+
+public class FirstTimeFragment extends DialogFragment {
+
+    public FirstTimeFragment() {
+        // Empty constructor is required for DialogFragment
+        // Make sure not to add arguments to the constructor
+        // Use `newInstance` instead as shown below
+    }
+
+    public static FirstTimeFragment newInstance(String title) {
+        FirstTimeFragment frag = new FirstTimeFragment();
+        Bundle args = new Bundle();
+        args.putString("title", title);
+        frag.setArguments(args);
+        return frag;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, final ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_activity_reminder, container);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button dismissButton = view.findViewById(R.id.first_time_dialog_ok_button);
+        dismissButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+    }
+}
